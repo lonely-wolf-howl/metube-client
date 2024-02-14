970982f05eeabab43bfd46f16d734c447f376f11
kme-developer <electruc0095@gmail.com>
2024-02-14 17:41:07 +0900

    feat. video detail 컴포넌트 구현 및 하나만 불러오는 로직 테스트 완료.

--- src/app/(home)/loading.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { CircularProgress } from '@nextui-org/react';
-
-export default function Loading() {
-  return <CircularProgress />;
-}
--- src/components/VideoDetail.tsx
+++ src/components/VideoDetail.tsx
@@ -1,13 +1,29 @@
 'use client';
 
-import React from 'react';
-import { Button, Card, CardHeader, CardBody } from '@nextui-org/react';
+import React, { useEffect, useState } from 'react';
+import {
+  Button,
+  Card,
+  CardHeader,
+  CardBody,
+  CircularProgress,
+} from '@nextui-org/react';
 import { IoMdDownload } from 'react-icons/io';
 import { signIn, useSession } from 'next-auth/react';
+import axios from 'axios';
+import { BACKEND_URL } from '../app/(home)/page';
 
 const VideoDetail = ({ id }: { id: string }) => {
   const { data: session } = useSession();
-  const source = `/videos/0${id}.mp4`;
+  const [video, setVideo] = useState('loading');
+
+  useEffect(() => {
+    const getVideo = async () => {
+      const response = await axios.get(`${BACKEND_URL}/api/videos/${id}`);
+      setVideo(response.data);
+    };
+    getVideo();
+  }, []);
 
   const checkSessionForDownload = () => {
     if (!session) {
@@ -19,30 +35,39 @@ const VideoDetail = ({ id }: { id: string }) => {
 
   return (
     <div>
-      <video
-        className="rounded-lg"
-        width="1080"
-        height="720"
-        controls
-        controlsList="nodownload"
-      >
-        <source src={source} type="video/mp4" />
-      </video>
-      <div className="flex gap-4 items-center">
-        <div>
-          <h4 className="font-bold text-large">동영상 제목</h4>
-          <small className="text-default-500 font-bold">
-            사용자 · 조회수 ?회
-          </small>
-        </div>
-        <div className="flex-grow"></div>
-        <div className="flex gap-4">
-          <Button radius="full" onClick={checkSessionForDownload}>
-            <IoMdDownload />
-            <span className="font-bold text-inherit">동영상 저장</span>
-          </Button>
-        </div>
-      </div>
+      {video === 'loading' ? (
+        <main className="flex flex-col justify-center items-center h-screen">
+          <CircularProgress aria-label="Loading..." />
+        </main>
+      ) : (
+        <Card className="py-4">
+          <CardBody className="overflow-visible py-2">
+            <video
+              className="rounded-lg"
+              width="720"
+              height="480"
+              controls
+              controlsList="nodownload"
+            >
+              <source src={video.source} type="video/mp4" />
+            </video>
+          </CardBody>
+          <CardHeader className="pb-0 pt-2 px-4 flex justify-between items-center">
+            <div>
+              <h4 className="font-bold text-large">{video.title}</h4>
+              <small className="text-default-500 font-bold">
+                {video.displayName} · 조회수 {video.viewCount}회
+              </small>
+            </div>
+            <div>
+              <Button radius="full" onClick={checkSessionForDownload}>
+                <IoMdDownload />
+                <span className="font-bold text-inherit">동영상 저장</span>
+              </Button>
+            </div>
+          </CardHeader>
+        </Card>
+      )}
     </div>
   );
 };
